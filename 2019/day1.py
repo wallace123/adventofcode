37b6bb930140ebd91ede976953156b744bbf39ea
wallace123 <wallace123@github.com>
2019-12-01 00:29:40 -0500

    2019 day1 done

--- 2019/day1.py
+++ 2019/day1.py
@@ -1,12 +1,52 @@
 #!/usr/bin/python3
+import math
 
 INPUT1 = 'data/day1'
+TEST1 = '14\n'
+TEST2 = '1969\n'
+TEST3 = '100756\n'
 
 with open(INPUT1) as f:
     data = f.read()
 
 def parse1(data):
-    pass
+    mass = []
+    mydata = data.split('\n')
+    del mydata[-1]
+    for line in mydata:
+        #print(line)
+        mass.append(int(line.strip()))
+    return mass
+  
 
-def parse2(data):
-    pass
+def calcmass(mass):
+    return (math.floor(mass/3))-2
+
+def calcfuel(mass):
+    fuel = 0
+    mass = calcmass(mass)
+    while mass > 0:
+        fuel = fuel + mass
+        mass = calcmass(mass)
+
+    return fuel
+
+def star1(data):
+    mass = parse1(data)
+    sum = 0
+    for item in mass:
+        sum = sum + calcmass(item)
+
+    print(sum)
+
+
+def star2(data):
+    mass = parse1(data)
+    sum = 0
+    for item in mass:
+        sum = sum + calcfuel(item)
+
+    print(sum)
+
+star1(data)
+star2(data)
