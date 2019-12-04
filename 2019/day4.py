e32510c1fca8e08420ce8cf58379f84b7a3c2c67
wallace123 <wallace123@github.com>
2019-12-04 05:50:32 -0500

    Finished day4 part1

--- 2019/day4.py
+++ 2019/day4.py
@@ -1,1 +1,77 @@
 #!/usr/bin/python3
+from collections import Counter
+
+INPUT1 = '387638-919123'
+TEST1 = '111111'  # TRUE
+TEST2 = '223450'  # FALSE
+TEST3 = '123789'  # FALSE
+TEST4 = '122345'  # TRUE
+
+def parse1(data):
+    nums = data.split('-')
+    tmp = []
+    if len(nums) == 2:
+        for i in range(int(nums[0]), int(nums[1])+1):
+            tmp.append(str(i))
+    else:
+        tmp = nums
+
+    return tmp
+
+def ispass(p):
+    # check length 6
+    if len(p) != 6:
+        #print('Failed length test: ', p)
+        return False
+    
+    # check double
+    doub = False
+    digcount = Counter(p)
+    for i in digcount.values():
+        if i > 1:
+            doub = True
+            break
+
+    if not doub:
+        #print('Failed double check: ', p)
+        return False
+
+    # check adjacent
+    adj = False
+    for i in range(0, len(p)+1):
+        try:
+            if p[i] == p[i+1]:
+                adj = True
+                break
+        except IndexError:
+            pass
+
+    if not adj:
+        #print('Failed adjacent check: ', p)
+        return False
+
+    # check never decrease
+    for i in range(0, len(p)+1):
+        try:
+            if p[i] > p[i+1]:
+                #print('Failed decrease check: ', p)
+                return False
+        except IndexError:
+            pass
+
+    return True
+
+def star1(data):
+    mydata = parse1(data)
+    count = 0
+    for item in mydata:
+        if ispass(item):
+            count += 1
+
+    print(count)
+
+def star2(data):
+    pass
+
+star1(INPUT1)
+star2(INPUT1)
