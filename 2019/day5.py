9636991455f3182b81c125ca246904d02e6040fd
wallace123 <wallace123@github.com>
2019-12-07 12:05:13 -0500

    updates for day5

--- 2019/day5.py
+++ 2019/day5.py
@@ -28,7 +28,7 @@ def getmodes(op):
     c.pop(0)
     return opcode, c
 
-
+#opcode = 1
 def add(eip, data, modes):
     p1 = data[eip+1]
     p2 = data[eip+2]
@@ -52,7 +52,7 @@ def add(eip, data, modes):
 
     return eip+4, data
 
-
+#opcode = 2
 def mul(eip, data, modes):
     p1 = data[eip+1]
     p2 = data[eip+2]
@@ -76,20 +76,127 @@ def mul(eip, data, modes):
 
     return eip+4, data
 
-
+#opcode = 3
 def inp(eip, myin, data, modes):
     p1 = data[eip+1]
     data[p1] = myin
 
     return eip+2, data
 
-
+#opcode = 4
 def getout(eip, data, modes):
     p1 = data[eip+1]
     out = data[p1]
 
     return eip+2, out
-    
+
+
+#opcode = 5
+def jit(eip, data, modes):
+    p1 = data[eip+1]
+    p2 = data[eip+2]
+
+    if modes[0] == 0:
+        p1 = data[p1]
+    elif modes[0] == 1:
+        p1 = p1
+    else:
+        print("Error")
+
+    if modes[1] == 0:
+        p2 == data[p2]
+    elif modes[1] == 1:
+        p2 = p2
+    else:
+        print("Error")
+
+    if p1 == 0:
+        return eip+3
+    else:
+        return p2
+
+
+#opcode = 6
+def jif(eip, data, modes):
+    p1 = data[eip+1]
+    p2 = data[eip+2]
+
+    if modes[0] == 0:
+        p1 = data[p1]
+    elif modes[0] == 1:
+        p1 = p1
+    else:
+        print("Error")
+
+    if modes[1] == 0:
+        p2 == data[p2]
+    elif modes[1] == 1:
+        p2 = p2
+    else:
+        print("Error")
+
+    if p1 == 0:
+        return p2
+    else:
+        return eip+3
+
+
+#opcode = 7
+def lt(eip, data, modes):
+    p1 = data[eip+1]
+    p2 = data[eip+2]
+    p3 = data[eip+3]
+
+    if modes[0] == 0:
+        p1 = data[p1]
+    elif modes[1] == 1:
+        p1 = p1
+    else:
+        print("Error")
+
+    if modes[1] == 0:
+        p2 = data[p2]
+    elif modes[1] == 1
+        p2 = p2
+    else:
+        print("Error")
+
+    if p1 < p2:
+        data[p3] = 1
+    else:
+        data[p3] = 0
+
+    return eip+4, data    
+
+
+#opcode = 8
+def eq(eip, data, modes):
+    p1 = data[eip+1]
+    p2 = data[eip+2]
+    p3 = data[eip+3]
+
+    if modes[0] == 0:
+        p1 = data[p1]
+    elif modes[1] == 1:
+        p1 = p1
+    else:
+        print("Error")
+
+    if modes[1] == 0:
+        p2 = data[p2]
+    elif modes[1] == 1
+        p2 = p2
+    else:
+        print("Error")
+
+    if p1 == p2:
+        data[p3] = 1
+    else:
+        data[p3] = 0
+
+    return eip+4, data    
+
+
 def comp(data, myin):
     eip = 0
     while data[eip] != 99:
@@ -103,6 +210,8 @@ def comp(data, myin):
         elif op == 4:
             eip, out = getout(eip, data, modes)
             print(out)
+        elif op == 5:
+            eip = 
 
     return data
 
