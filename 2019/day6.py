88e08fe8a122c212b6c68dcd591c0e71a5c75685
wallace123 <wallace123@github.com>
2019-12-08 07:06:00 -0500

    updates day6

--- 2019/day6.py
+++ 2019/day6.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import io
+
 
 INPUT1 = 'data/day6'
 TEST1 = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L" # 42
@@ -8,8 +10,13 @@ with open(INPUT1) as f:
 
 
 def parse1(data):
-    items = data.split('\n')
-    return items
+    items = io.StringIO(data)
+    items = items.readlines()
+
+    mylist = []
+    for i in items:
+        mylist.append((i.split(')')[0].strip(), i.split(')')[1].strip()))
+    return mylist
 
 
 def star1(data):
@@ -21,5 +28,5 @@ def star2(data):
     pass
 
 
-star1(data)
+star1(TEST1)
 star2(data)
