75aa4c8d1a2283b1aeb6d0ed5c6e8bbcf141310b
wallace123 <wallace123@github.com>
2019-12-09 17:21:06 -0500

    start to day9

--- 2019/day9.py
+++ 2019/day9.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+INPUT1 = 'data/day9'
+
+with open(INPUT1) as f:
+    data = f.read()
+
+
+def parse1(data):
+    codes = data.split(',')
+    nums = []
+    for item in codes:
+        nums.append(int(item.strip()))
+    return nums
+
+
+def star1(data):
+    mydata = parse1(data)
+    print(mydata)
+
+
+def star2(data):
+    pass
+
+
+star1(data)
+star2(data)
