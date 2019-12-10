092e3b6a14a083d5be92af429e77844814e0bcdf
wallace123 <wallace123@github.com>
2019-12-10 07:26:43 -0500

    adding input for day10. Prob a skip for me

--- 2019/day10.py
+++ 2019/day10.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+INPUT1 = 'data/day10'
+
+with open(INPUT1) as f:
+    data = f.read()
+
+
+def parse1(data):
+    pass
+
+
+def star1(data):
+    pass
+
+
+def star2(data):
+    pass
+
+
+star1(data)
+star2(data)
