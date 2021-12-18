f41d8e4722ce4105d50fa2939334ffb490ef6330
wallace123 <wallace123@github.com>
2021-12-18 11:22:48 -0500

    starting day 17

---
+++ 2021/day17.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+class Trench:
+    def __init__(self):
+        self.target_area = []
+        self.x_range = (0, 0)
+        self.y_range = (0, 0)
+        
+    def _parse_input(self):
+        with open('data/day17.txt', 'r') as infile:
+            content = infile.read().rstrip()
+            
+        content = content.split(':')
+        vals = content[1].strip().split(',')
+        x = vals[0].split('=')[1]
+        y = vals[1].split('=')[1]
+        xr = x.split('..')
+        yr = y.split('..')
+        self.x_range = (int(xr[0]), int(xr[1]))
+        self.y_range = (int(yr[0]), int(yr[1]))
+        
+        for xi in range(self.x_range[0], self.x_range[1]+1):
+            for yi in range(self.y_range[0], self.y_range[1]+1):
+                self.target_area.append((xi, yi))
+
+    def step(self, position, velocity):
+        x = velocity[0]
+        y = velocity[1]
+        
+        new_position = (position[0] + x, position[1] + y)
+        
+        if x > 0:
+            new_x = x - 1
+        elif x < 0:
+            new_x = x + 1
+        else:
+            new_x = x
+
+        new_y = y - 1
+        
+        new_velocity = (new_x, new_y)
+        
+        return new_position, new_velocity
+        
+    def hit(self, coord):
+        if coord in self.target_area:
+            return True
+        else:
+            return False
+            
+    def over(self, coord):
+        x = coord[0]
+        y = coord[1]
+        
+        if y < self.y_range[0]:
+            return True
+        
+    def shoot(self, velocity):
+        position = (0, 0)
+        while 1:
+            position, velocity = self.step(position, velocity)
+            if self.hit(position):
+                print('hit!')
+                return
+            elif self.over(position):
+                print('over!')
+                return       
+
+
+if __name__ == '__main__':
+    T = Trench()
+    T._parse_input()
+    T.shoot((17, -4))
